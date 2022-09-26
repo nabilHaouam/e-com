69a539ce9c045803ee6db7d20db8d7f7c4d44ac9
nabilHaouam <workmailnabil@gmail.com>
2022-09-27 00:35:01 +0100

    handled sign-out issue

--- src/firebase.js
+++ src/firebase.js
@@ -35,6 +35,7 @@ export const signInWithGoogle = () =>
       const token = credential.accessToken;
       // The signed-in user info.
       const user = result.user;
+      window.location = "/";
       window.location = "/#/shop";
       // ...
     })
@@ -53,6 +54,7 @@ export const signOutFromApp = () =>
   signOut(auth)
     .then(() => {
       // Sign-out successful.
+      window.location = "/";
       window.location = "/#/sign-in-sign-up";
     })
     .catch((error) => {
